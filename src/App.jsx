0bcf6a5d1475993897c7c1d42cf4f1d46c7e0baf
feliun <felipe.polo@gmail.com>
2018-05-29 17:19:10 +0200

    Building deadline - countdown application

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+import './App.css';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			deadline: '25 December, 2019',
+			newDeadline: '',
+		};
+	}
+
+	changeDeadline() {
+		this.setState({
+			deadline: this.state.newDeadline,
+			newDeadline: '',
+		});
+	}
+
+	render() {
+		return (
+			<div id="app">
+				<div id="title">Countdown to {this.state.deadline}</div>
+				<Clock deadline={this.state.deadline} />
+				<div>
+					<input
+						id="deadline-input"
+						onChange={event => this.setState({ newDeadline: event.target.value })}
+						placeholder="Enter a valid date..."
+					/>
+					<button onClick={() => this.changeDeadline()}>Submit</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
